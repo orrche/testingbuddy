a7b71df7e1faab3cfd861b98a7efed53daf2d119
Kent Gustavsson <kent.gustavsson@jordbruksverket.se>
2019-07-19 13:09:01 +0200

    Second commit :)
    Signed-off-by: Kent Gustavsson <kent.gustavsson@jordbruksverket.se>

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/BurntSushi/toml"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	type ta struct {
+		Name string
+	}
+
+	t := ta{}
+	toml.DecodeFile("test.toml", &t)
 }
 
 func main() {
